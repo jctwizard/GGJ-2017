fccee3698860d42d22641238c720464b40aa54e6
Ade Cross <afroade@hotmail.com>
2017-01-20 23:05:49 +0000

    added wave movement script

---
+++ Assets/Scripts/WaveObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveObject : MonoBehaviour {
+
+
+    public bool offScreen;
+    public float horizontalSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 newPosition = transform.position;
+
+        newPosition.x += horizontalSpeed * Time.deltaTime;
+
+        transform.position = newPosition;
+    }
+
+    void OnBecameInvisible()
+    {
+        offScreen = true;
+    }
+}
