0783db9e5125e079debd278b4c1ae29b670433eb
Ade Cross <afroade@hotmail.com>
2017-01-20 20:36:11 +0000

    added wave movement script

---
+++ Assets/Scripts/WaveMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveMovement : MonoBehaviour {
+
+    public float timeOffSet;
+    public float heightOffSet;
+
+    private float orginalHeight;
+
+	// Use this for initialization
+	void Start ()
+    {
+        orginalHeight = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float newHeight;
+
+        newHeight = Mathf.Sin(Time.time);
+
+        newHeight += orginalHeight;
+
+        newHeight += heightOffSet;
+
+        Vector3 newPosition = transform.position;
+
+        newPosition.y = newHeight;
+
+        transform.position = newPosition;
+        	
+	}
+}
